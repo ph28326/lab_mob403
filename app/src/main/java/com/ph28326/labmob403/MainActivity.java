11ee623425d7779ca11cada958f8838295889f1b
Khánh Toàn <toantqkph28326@fpt.edu.vn>
2023-06-27 21:57:28 +0700

    lab1_bai1

--- app/src/main/java/com/ph28326/labmob403/MainActivity.java
+++ app/src/main/java/com/ph28326/labmob403/MainActivity.java
@@ -2,7 +2,18 @@ package com.ph28326.labmob403;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLoad = findViewById(R.id.btnLoad);
+
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DownloadImageThread thread = new DownloadImageThread();
+                thread.start();
+            }
+        });
+
+    }
+    private class DownloadImageThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                URL url = new URL("https://picsum.photos/536/354");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                final Bitmap bitmap = BitmapFactory.decodeStream(input);
+
+                // Update UI on the main thread
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageView imageView = findViewById(R.id.imgAndroid);
+                        imageView.setImageBitmap(bitmap);
+                        TextView textView = findViewById(R.id.tvMessage);
+                        textView.setText("Image Downloaded");
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
