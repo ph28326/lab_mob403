8569a9d1711c1b99485c1a76de81dd13c0a3228f
Khánh Toàn <toantqkph28326@fpt.edu.vn>
2023-06-27 23:43:05 +0700

    lab1_bai2,3,4

---
+++ app/src/main/java/com/ph28326/labmob403/Bai4.java
@@ -0,0 +1,75 @@
+package com.ph28326.labmob403;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Bai4 extends AppCompatActivity {
+    private EditText edTime;
+    private Button btnRun;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai4);
+
+        edTime = (EditText) findViewById(R.id.edTime);
+        btnRun = (Button) findViewById(R.id.btnRun);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Bài 4");
+        progressDialog.setProgressStyle(androidx.appcompat.R.style.Base_Widget_AppCompat_ProgressBar_Horizontal);
+        progressDialog.setProgress(0);
+
+        AsyncTask<Integer, Integer, Boolean> asyncTask = new AsyncTask<Integer, Integer, Boolean>() {
+            @Override
+            protected Boolean doInBackground(Integer... integers) {
+                int second = integers[0];
+                try {
+                    for (int i = second; i > 0; i--) {
+                        publishProgress(i);
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                return true;
+            }
+
+            @Override
+            protected void onPostExecute(Boolean aBoolean) {
+                super.onPostExecute(aBoolean);
+                if (progressDialog.isShowing() && aBoolean) {
+                    progressDialog.dismiss();
+                }
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                super.onProgressUpdate(values);
+                progressDialog.setMessage("Please wait " + values[0]);
+                progressDialog.incrementProgressBy(1);
+            }
+        };
+
+        btnRun.setOnClickListener(v -> {
+            String input = edTime.getText().toString();
+            if (input.isEmpty()) {
+                Toast.makeText(this, "Please enter a number", Toast.LENGTH_SHORT).show();
+            } else {
+                int seconds = Integer.parseInt(input);
+                progressDialog.setMax(seconds);
+                progressDialog.setMessage("Please wait " + seconds);
+                progressDialog.show();
+                asyncTask.execute(seconds);
+            }
+        });
+    }
+}
+
